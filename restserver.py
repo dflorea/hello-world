377717b085d0345c8ae0588ad70abf320cc349cf
jacobffjensen <47257511+jacobffjensen@users.noreply.github.com>
2019-02-08 13:23:55 -0500

    a small server

--- restserver.py
+++ restserver.py
@@ -1,6 +1,7 @@
 # The is the REST API server side code
 # Jacob Jensen - copyright 2019
 # DANJACOB project
+# pip install flask flask_restful
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
