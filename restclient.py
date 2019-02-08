ee51c5dedadac9f7bcee414d394600b9267e92bc
jacobffjensen <47257511+jacobffjensen@users.noreply.github.com>
2019-02-08 14:27:03 -0500

    update to client

---
+++ restclient.py
@@ -0,0 +1,22 @@
+# The is the REST API server side code
+# Jacob Jensen - copyright 2019
+# DANJACOB project
+# pip install flask flask_restful Flask-Jsonpify
+# pip install Flask-MySQL
+
+# http://35.175.141.30:5002/pictures
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
+from json import dumps
+from flask.ext.jsonpify import jsonify
+
+import requests
+
+resp = requests.get('http://52.207.248.24:5002/picture')
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('ERROR - RESPONSE FROM SERVER {}'.format(resp.status_code))
+for respitem in resp.json():
+    print(respitem)
