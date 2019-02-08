366df0b6c9621cc3cbb6755e87088d954aa479e1
jacobffjensen <47257511+jacobffjensen@users.noreply.github.com>
2019-02-08 14:38:19 -0500

    updates to client server

--- restserver.py
+++ restserver.py
@@ -5,7 +5,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
-from flask.ext.jsonpify import jsonify
+from flask import jsonify
 
 app = Flask(__name__)
 api = Api(app)
