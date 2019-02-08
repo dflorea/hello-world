f5ebec1e856e7644576228723871c5aded5d8b0e
jacobffjensen <47257511+jacobffjensen@users.noreply.github.com>
2019-02-08 12:57:14 -0500

    added REST API server

---
+++ restserver.py
@@ -0,0 +1,21 @@
+# The is the REST API server side code
+# Jacob Jensen - copyright 2019
+# DANJACOB project
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+#from flask.ext.jsonpify import jsonify
+
+app = Flask(__name__)
+api = Api(app)
+
+class Picture(Resource):
+    def get(self):
+        return {'nothing yet'}
+        #return jsonify(result)
+
+api.add_resource(Picture, '/picture') # Route_1
+
+if __name__ == '__main__':
+     app.run(port='5002')
+     
